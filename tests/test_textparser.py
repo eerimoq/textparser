83eb8c2fbd90880c2b76bc2d700914df010bda65
Erik Moqvist <erik.moqvist@gmail.com>
2018-07-22 09:27:39 +0200

    Test or zero and one or more.

--- tests/test_textparser.py
+++ tests/test_textparser.py
@@ -3,6 +3,8 @@ import unittest
 import textparser
 from textparser import Grammar
 from textparser import Sequence
+from textparser import OneOrMore
+from textparser import ZeroOrMore
 from textparser import DelimitedList
 from textparser import Token
 from textparser import TokenizerError
@@ -49,6 +51,49 @@ class TextParserTest(unittest.TestCase):
             tokens = tokenize(tokens + [('__EOF__', '')])
             tree = grammar.parse(tokens)
             self.assertEqual(tree, expected_tree)
+
+    def test_zero_or_more(self):
+        grammar = Grammar(ZeroOrMore('WORD'))
+
+        datas = [
+            ([], []),
+            ([('WORD', 'foo')], ['foo']),
+            ([('WORD', 'foo'), ('WORD', 'bar')], ['foo', 'bar'])
+        ]
+
+        for tokens, expected_tree in datas:
+            tokens = tokenize(tokens + [('__EOF__', '')])
+            tree = grammar.parse(tokens)
+            self.assertEqual(tree, expected_tree)
+
+    def test_one_or_more(self):
+        grammar = Grammar(OneOrMore('WORD'))
+
+        datas = [
+            ([('WORD', 'foo')], ['foo']),
+            ([('WORD', 'foo'), ('WORD', 'bar')], ['foo', 'bar'])
+        ]
+
+        for tokens, expected_tree in datas:
+            tokens = tokenize(tokens + [('__EOF__', '')])
+            tree = grammar.parse(tokens)
+            self.assertEqual(tree, expected_tree)
+
+    def test_one_or_more_mismatch(self):
+        grammar = Grammar(OneOrMore('WORD'))
+
+        datas = [
+            [],
+            [('NUMBER', 'foo')]
+        ]
+
+        for tokens in datas:
+            tokens = tokenize(tokens + [('__EOF__', '')])
+
+            with self.assertRaises(textparser.Error) as cm:
+                grammar.parse(tokens)
+
+            self.assertEqual(str(cm.exception), '')
 
     def test_tokenizer_error(self):
         datas = [
